a3d1e31cdf3c187964ecf7316f0cc6d82bf4ada8
Paul McGrath <paul.mcgrath@hostelworld.com>
2016-01-19 17:17:04 +0000

    Small tweaks

--- NYTBrowser/DetailViewController.swift
+++ NYTBrowser/DetailViewController.swift
@@ -13,7 +13,8 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
-    var detailItem: AnyObject? {
+
+    var detailItem: Report? {
         didSet {
             // Update the view.
             self.configureView()
@@ -24,7 +25,7 @@ class DetailViewController: UIViewController {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
             if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+                label.text = detail.abstract
             }
         }
     }
--- NYTBrowser/MasterViewController.swift
+++ NYTBrowser/MasterViewController.swift
@@ -62,7 +62,7 @@ class MasterViewController: UITableViewController {
             if let indexPath = self.tableView.indexPathForSelectedRow {
                 let object = objects[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                //controller.detailItem = object
+                controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
